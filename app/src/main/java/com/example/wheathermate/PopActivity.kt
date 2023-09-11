6cdb2866ff4f9628aaf650c4cd88e2d7e74f7cd4
Oh-JunTaek <aa39043533@gmail.com>
2023-09-11 10:11:05 +0900

    WheatherMate

---
+++ app/src/main/java/com/example/wheathermate/PopActivity.kt
@@ -0,0 +1,11 @@
+package com.example.wheathermate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PopActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pop)
+    }
+}
