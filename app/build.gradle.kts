8b7b572c1821f32aad7e839b740300cdef342066
Oh-JunTaek <aa39043533@gmail.com>
2023-09-12 17:39:21 +0900

    WheatherMate

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,6 +20,10 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    buildFeatures {
+        buildConfig = true
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -44,6 +48,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.gms:play-services-auth:19.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/wheathermate/SplashActivity.kt
+++ app/src/main/java/com/example/wheathermate/SplashActivity.kt
@@ -1,23 +1,66 @@
 package com.example.wheathermate
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 
 class SplashActivity : AppCompatActivity() {
+
+    companion object{
+        private const val RC_SIGN_IN= 1234 // 혹은 다른 숫자.
+        private const val TAG= "SplashActivity"
+    }
+
+    private lateinit var googleSignInClient: GoogleSignInClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        // 로딩이 끝난 후, HomeActivity를 실행하도록 합니다.
-        Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
 
-            // HomeActivity를 시작한 후에는 SplashActivity를 종료합니다.
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        val signInButton = findViewById<com.google.android.gms.common.SignInButton>(R.id.sign_in_button)
+        signInButton.setOnClickListener {
+            val signInIntent = googleSignInClient.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleGoogleSignInResult(task)
+        }
+    }
+
+    private fun handleGoogleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+
+            // 성공적으로 로그인한 경우 여기서 원하는 동작 수행 (예: HomeActivity로 넘어감)
+            startActivity(Intent(this@SplashActivity, HomeActivity::class.java))
             finish()
-        }, 4000)  // 4000ms(4초) 후에 run() 메서드를 실행합니다.
+
+        } catch (e: ApiException) {
+            Log.e(TAG, "Google sign in failed", e)
+
+            // 로그인 실패 시 예외 처리 (예 : 다시 Splash 화면에서 시작 등의 작업 수행 가능.)
+        }
     }
 }
