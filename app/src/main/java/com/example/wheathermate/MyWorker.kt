dffac0bb594f84834368be9ab56a8f88d97fd849
Eu <aa39043533@gmail.com>
2023-09-28 17:07:18 +0900

    worker

---
+++ app/src/main/java/com/example/wheathermate/MyWorker.kt
@@ -0,0 +1,18 @@
+package com.example.wheathermate
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class MyWorker(appContext: Context, workerParams: WorkerParameters)
+    : Worker(appContext, workerParams) {
+
+    override fun doWork(): Result {
+        // Do the work here
+
+        // Indicate whether the work finished successfully with the Result
+        return Result.success()
+    }
+}
