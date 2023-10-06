3000e96089759f10a00a3ab8f6c39961b2a2140b
Oh-JunTaek <143782929+Oh-JunTaek@users.noreply.github.com>
2023-10-07 02:27:47 +0900

    WheatherMate

--- app/src/main/java/com/example/wheathermate/RsettingFragment.kt
+++ app/src/main/java/com/example/wheathermate/RsettingFragment.kt
@@ -1,5 +1,6 @@
 package com.example.wheathermate
 
+import android.content.Context
 import androidx.fragment.app.Fragment
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,8 +11,8 @@ import com.example.wheathermate.databinding.FragmentRsettingBinding
 
 class RsettingFragment : Fragment() {
 
-    private var _binding: FragmentRsettingBinding? = null
-    private val binding get() = _binding!!
+    private var _binding: FragmentRsettingBinding? = null // view binding instance.
+    private val binding get() = _binding!! // non-null assertion for the view binding.
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,12 +21,25 @@ class RsettingFragment : Fragment() {
     ): View? {
         _binding = FragmentRsettingBinding.inflate(inflater, container, false)
 
-        // 각 스위치에 대한 리스너 설정 및 초기 상태 설정 등을 여기서 합니다.
+        // Get the saved switch state from SharedPreferences.
+        val sharedPreferences = requireActivity().getPreferences(Context.MODE_PRIVATE)
+        val savedSwitchState = sharedPreferences.getBoolean("switch1", false)
+
+        // Set the initial switch state.
+        binding.switch1.isChecked = savedSwitchState
+
+        // Set a listener for switch1.
         binding.switch1.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
-                Toast.makeText(context, "Switch 1 is on", Toast.LENGTH_SHORT).show()
+                Toast.makeText(context, "알림을 보낼게요", Toast.LENGTH_SHORT).show()
             } else {
-                Toast.makeText(context, "Switch 1 is off", Toast.LENGTH_SHORT).show()
+                Toast.makeText(context, "알림을 껐어요", Toast.LENGTH_SHORT).show()
+            }
+
+            // Save the new switch state to SharedPreferences.
+            with(sharedPreferences.edit()) {
+                putBoolean("switch1", isChecked)
+                apply()
             }
         }
 
